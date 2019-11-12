2a1480313330ffbf8a0c962ae5fb2daca18275ab
Tahir Raza <tahir.raza@ilsainteractive.com>
2019-11-12 17:07:31 +0500

    2 solutions

--- custom/plugins/ETribesCategoryImport/Components/ETribesCategoryImport/Utils/ImportCommandHelper.php
+++ custom/plugins/ETribesCategoryImport/Components/ETribesCategoryImport/Utils/ImportCommandHelper.php
@@ -37,6 +37,10 @@ class ImportCommandHelper
         $_preparared_model['categoryName'] = $val;
 
         switch ($key) {
+            case 'lang':
+                // TODO: add from constant
+                $_preparared_model['parentName'] = 'Root';
+                break;
             case 'product_line_area':
                 $_preparared_model['parentName'] = $record['lang'];
                 break;
@@ -56,20 +60,14 @@ class ImportCommandHelper
     {
         $records = json_decode($this->categoriesData, true);
         $this->categoriesData = [];
-        $prepared_record = [];
         foreach($records as $record) {
             if(!isset($record['lang'])){
                 $record['lang'] = 'en';
             }
-            if(!isset($prepared_record[$record['lang']])){
-                $prepared_record[$record['lang']] = [];
-            }
             foreach ($record as $key => $val) {
-                if ($key != 'lang')
-                    array_push($prepared_record[$record['lang']], $this->getPreparedRecord($key, $val, $record));
+                array_push($this->categoriesData, $this->getPreparedRecord($key, $val, $record));
             }
         }
-        print_r($prepared_record);die;
     }
 
     public function importAction()
